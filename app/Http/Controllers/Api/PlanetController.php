b114e0b5e961d2b1b7c3bf19ae1997eda9fc5508
Max McBurns <max.mcburns@gmail.com>
2022-04-25 01:57:05 +0300

    Add planet form.

--- app/Http/Controllers/Api/PlanetController.php
+++ app/Http/Controllers/Api/PlanetController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Planets;
 use App\Services\Response\ResponseService;
 use App\Services\Section\Planet\PlanetService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PlanetController extends ApiController
 {
@@ -36,7 +38,7 @@ class PlanetController extends ApiController
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -46,30 +48,45 @@ class PlanetController extends ApiController
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Planets $planet
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Planets $planet): JsonResponse
     {
         //
+        $planet = $this->service->store($request, $planet);
+
+        return ResponseService::sendJsonResponse(
+            true,
+            [
+                'item' => $planet->toArray()
+            ]
+        );
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Planets $planet
+     * @return JsonResponse
      */
-    public function show($id)
+    public function show(Planets $planet): JsonResponse
     {
         //
+        return ResponseService::sendJsonResponse(
+            true,
+            [
+                'item' => $planet->toArray()
+            ]
+        );
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -79,9 +96,9 @@ class PlanetController extends ApiController
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -92,7 +109,7 @@ class PlanetController extends ApiController
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
